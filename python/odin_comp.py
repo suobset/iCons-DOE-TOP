1caf968eeefb2a3e8b20348416bafd6110e49add
suobset <kushagra2410@gmail.com>
2023-12-08 10:36:39 -0500

    Add map to actual prod

--- python/odin_comp.py
+++ python/odin_comp.py
@@ -3,10 +3,13 @@ import folium.plugins
 import pandas as pd
 import json
 from urllib.request import urlopen
+import numpy as np
+from folium.plugins import MiniMap
 
 # Read CSV files
-actual_odin = pd.read_csv("./data/actual_odin.csv")
-predicted_odin = pd.read_csv("./data/predicted_odin.csv")
+actual_odin = pd.read_csv("../data/actual_odin.csv")
+predicted_odin = pd.read_csv("../data/predicted_odin.csv")
+predicted_complete = pd.read_csv("../data/predicted_complete.csv")
 
 # Load GeoJSON data for counties
 with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
@@ -16,9 +19,9 @@ with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-c
 with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
     states = json.load(response)
 
-# Create a Folium map centered at an initial location
-map_center = [37.7749, -122.4194]  # You can adjust these coordinates
-m = folium.Map(location=map_center, zoom_start=10)
+# Set the center to the geographical center of the United States
+map_center = [39.8283, -98.5795]
+m = folium.Map(location=map_center, zoom_start=4)
 
 # Add actual ODIN choropleth layer to the map
 folium.Choropleth(
@@ -52,6 +55,30 @@ folium.Choropleth(
     tooltip=folium.GeoJsonTooltip(fields=['name'], labels=False)
 ).add_to(m)
 
+# Create a new DataFrame without values over 1.2
+filtered_dataframe = predicted_complete[predicted_complete['F-VALUES'] <= 0.8]
+
+# Create a new column for fill_color based on the condition
+filtered_dataframe['fill_color'] = filtered_dataframe['F-VALUES']
+
+# Add predicted ODIN choropleth layer to the map
+folium.Choropleth(
+    geo_data=counties,
+    name='Power Burden',
+    data=filtered_dataframe,
+    columns=['FIPS', 'F-VALUES'],  # Update column names
+    key_on='feature.id',  # Adjust this according to your GeoJSON structure
+    fill_color='RdBu',
+    fill_opacity=0.7,
+    line_opacity=0.2,
+    nan_fill_color='gray',  # Set color for null values
+    nan_fill_opacity=0.5,  # Set opacity for null values
+    bins=[0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8],
+    legend_name='Power Burden',
+    tooltip=folium.GeoJsonTooltip(fields=['name'], labels=False)
+).add_to(m)
+
+
 # Add state borders with a higher line weight
 folium.GeoJson(
     states,
@@ -60,6 +87,9 @@ folium.GeoJson(
 
 # Add layer control for selecting layers
 folium.LayerControl().add_to(m)
+
+# Move the zoom control to the center left
+folium.plugins.MiniMap(toggle_display=True, position='bottomleft').add_to(m)
 
 # Display the map
 m.save('./actual_odin_map.html')
